94e3266abd98672dd1d77e5264c02e66ba039866
chennaiqiang <chennaiqiang@shule.com>
2016-08-23 13:11:41 +0800

    initial commit

---
+++ src/com/wx/publicNo/servlet/WechatServlet.java
@@ -0,0 +1,159 @@
+﻿package com.wx.publicNo.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wx.publicNo.process.WechatProcess;
+
+/**
+ * 微信服务端收发消息接口
+ * 
+ * @author pamchen-1 
+ * 
+ */
+public class WechatServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	public static final String Token = "token";
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 用来接收微信公众平台的验证 This method is called when a form has its tag value method
+	 * equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		try {
+			String signature = request.getParameter("signature");
+			String timestamp = request.getParameter("timestamp");
+			String nonce = request.getParameter("nonce");
+			String[] ArrTmp = { Token, timestamp, nonce };
+			Arrays.sort(ArrTmp);
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < ArrTmp.length; i++) {
+				sb.append(ArrTmp[i]);
+			}
+			String pwd = Encrypt(sb.toString());
+			String echostr = request.getParameter("echostr");
+			System.out.println("pwd==" + pwd);
+			System.out.println("echostr==" + echostr);
+			if (pwd.equals(signature)) {
+				if (!"".equals(echostr) && echostr != null) {
+					response.getWriter().print(echostr);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 用来向普通用户传送信息 This method is called when a form has its tag value method
+	 * equals to post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		/** 读取接收到的xml消息 */
+		StringBuffer sb = new StringBuffer();
+		InputStream is = request.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			sb.append(s);
+		}
+		String xml = sb.toString(); // 次即为接收到微信端发送过来的xml数据
+
+		String result = "";
+		/** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */
+		String echostr = request.getParameter("echostr");
+		if (echostr != null && echostr.length() > 1) {
+			result = echostr;
+		} else {
+			// 正常的微信处理流程
+			result = new WechatProcess().processWechatMag(xml);
+		}
+		try {
+			OutputStream os = response.getOutputStream();
+			os.write(result.getBytes("UTF-8"));
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String Encrypt(String strSrc) {
+		MessageDigest md = null;
+		String strDes = null;
+		byte[] bt = strSrc.getBytes();
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			md.update(bt);
+			strDes = bytes2Hex(md.digest()); // to HexString
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Invalid algorithm.");
+			return null;
+		}
+		return strDes;
+	}
+
+	public String bytes2Hex(byte[] bts) {
+		String des = "";
+		String tmp = null;
+		for (int i = 0; i < bts.length; i++) {
+			tmp = (Integer.toHexString(bts[i] & 0xFF));
+			if (tmp.length() == 1) {
+				des += "0";
+			}
+			des += tmp;
+		}
+		return des;
+	}
+
+	public boolean isChinese(String str) {
+		boolean result = false;
+		for (int i = 0; i < str.length(); i++) {
+			int chr1 = (char) str.charAt(i);
+			if (chr1 >= 19968 && chr1 <= 171941) {// 汉字范围\u4e00-\u9fa5 (中文)
+				result = true;
+			}
+		}
+		return result;
+	}
+}
